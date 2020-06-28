c4b287ad238cd1fbddc52563a9d228f65df5ec3b
savalone47 <savalonescott47@gmail.com>
2020-06-28 15:26:36 +0200

    charset method

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-import server from './config/server';
+import server from '../config/server';
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`app running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cors());
 
 export default server;
